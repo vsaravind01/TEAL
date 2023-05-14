e454927a02964b29b5e0b6b1b70840964f2624fc
vsaravind01 <vsaravind01@gmail.com>
2023-05-14 19:40:39 +0530

    Fixed extraction issues

--- a.py
+++ a.py
@@ -3,7 +3,23 @@ from bs4 import BeautifulSoup
 import json
 
 # Define the tags to scrape
-tags = ['Data Science', 'NLP', 'Deep Learning', 'Machine Learning', 'Artificial Intelligence']
+tags = ['Data Science', 'NLP', 'Deep Learning',
+        'Machine Learning', 'Artificial Intelligence']
+
+# Article
+ARTICLE_TAG = 'article'
+
+# Title
+TITLE_TAG = 'h2'
+
+# URL
+URL_TAG = 'a'
+
+# Author
+AUTHOR_TAG = 'p'
+
+# Date
+DATE_SELECTOR = 'article > div > div > div > div > div > div.je.jf.jg.jh.ji.ab > div.jn.bg.ab.jo > div.l.ee > span > div > a > p > span:nth-child(2)'
 
 # Initialize an empty list to store the scraped data
 articles = []
@@ -12,52 +28,51 @@ articles = []
 for tag in tags:
     # Construct the URL for the tag page
     url = f'https://medium.com/tag/{tag.lower().replace(" ", "-")}/top/all-time'
-    
+
+    print("Scraping data for tag: ", tag)
     # Make a request to the URL and get the page content
     response = requests.get(url)
     if response.status_code != 200:
-        print(f'Request failed with status code {response.status_code}')
-        exit()
+        raise RuntimeError(
+            f'Request failed with status code {response.status_code} for tag - {tag}')
     page_content = response.content
-    
+    print("Webpage content fetched successfully")
+
     # Parse the page content using BeautifulSoup
     soup = BeautifulSoup(page_content, 'html.parser')
-    
+
     # Find all the article elements on the page
     # Find all the article elements on the page
-    article_elements = soup.find_all('div', {'class': 'l'})
-    #article_elements = soup.select('.l .article')
-    print(article_elements)
-
-
+    article_elements = soup.find_all(ARTICLE_TAG)
+    # article_elements = soup.select('.l .article')
     # Loop through the article elements and extract the relevant data
-    for article_element in article_elements:
+    print(f'Found {len(article_elements)} articles for tag - {tag}')
+    print("Extracting article data")
+    for i, article_element in enumerate(article_elements):
         # Extract the title
-        title_element = article_element.find('h2')
+        title_element = article_element.find(TITLE_TAG)
         if title_element:
             title = title_element.text.strip()
         else:
             title = None
-        
+
         # Extract the article URL
-        url_element = article_element.find('a')
+        url_element = article_element.find(URL_TAG)
         if url_element is not None:
             url = url_element['href']
         else:
             url = None
 
-        
         # Extract the author name
         try:
-            author = article_element.find('p').text.strip()
+            author = article_element.find(AUTHOR_TAG).text.strip()
         except AttributeError:
             author = 'No author listed'
-        
+
         # Extract the date published
-        date_element = article_element.find('p', {'class': 'be b bf z dl'})
+        date_element = article_element.select(DATE_SELECTOR)
         if date_element:
-            date_span_elements = date_element.find_all('span')
-            date = ' '.join([d.text for d in date_span_elements])
+            date = date_element[0].text
         else:
             date = None
 
@@ -69,6 +84,8 @@ for tag in tags:
             'date': date,
             'tag': tag
         })
+    print('Finished extraction')
+    print('----------------------')
 
 # Write the scraped data to a JSON file
 with open('medium_articles.json', 'w') as f:
